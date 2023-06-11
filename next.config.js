8ef1ba6e283cf4a7b23ee485f7a03a014d9d7263
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2023-06-12 03:14:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   // Change the port to 3001 to avoid conflicts with the API server
   experimental: {
     appDir: true
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
